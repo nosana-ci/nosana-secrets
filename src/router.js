2de77a608aa65e6b24ed0377953a6d59cc933091
Laurens Verspeek <l.verspeek@gmail.com>
2022-11-03 10:16:10 +0100

    decrypt and receive secrets

--- src/router.js
+++ src/router.js
@@ -2,27 +2,16 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-const config = require('./generic/config');
-const { Kevast } = require('kevast');
-const { KevastFile } = require('kevast-file');
-const { KevastEncrypt } = require('kevast-encrypt');
-
-
 const authenticated = require('./middleware/auth');
 const authController = require('./controller/auth');
 const secretController = require('./controller/secret');
 
 router.post('/login', authController.login);
+router.get('/secrets', secretController.getSecrets);
 
 // Public routes
 router.get('/', async ctx => {
-    const fileStore = new KevastFile('./secrets/test.json');
-    const kevast = new Kevast(fileStore);
-    kevast.use(new KevastEncrypt(config.encryptionKey));
-    await kevast.set('key2', 'test');
-    const value = await kevast.get('key');
-
-    ctx.ok(value);
+    ctx.ok('Welcome to Nosana Secret Manager');
 });
 
 router.get('/health', async ctx => {
