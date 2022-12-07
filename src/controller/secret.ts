e5657e3b64f6207ebd9e2a3691a219fc1e2cfc6e
Laurens Verspeek <l.verspeek@gmail.com>
2022-12-07 11:20:14 +0100

    fix make connection

--- src/controller/secret.ts
+++ src/controller/secret.ts
@@ -2,22 +2,27 @@ import config from '../generic/config';
 
 import Keyv from 'keyv';
 
+const makeConnection = (address: string) => {
+  let storage;
+  if (config.storageConnection.includes('docdb.amazonaws.com')) {
+    storage = new Keyv(config.storageConnection, {
+      namespace: address,
+      tlsCAFile: '/app/rds-combined-ca-bundle.pem',
+    });
+  } else {
+    storage = new Keyv(config.storageConnection, {
+      namespace: address,
+    });
+  }
+  return storage;
+};
+
 export default {
   setSecrets: async (ctx: any) => {
     const { secrets } = ctx.request.body;
     const { user } = ctx.state;
     const address = user.address;
-    let storage;
-    if (config.storageConnection.includes('docdb.amazonaws.com')) {
-      storage = new Keyv(config.storageConnection, {
-        namespace: address,
-        tlsCAFile: '/app/rds-combined-ca-bundle.pem',
-      });
-    } else {
-      storage = new Keyv(config.storageConnection, {
-        namespace: address,
-      });
-    }
+    const storage = makeConnection(address);
 
     for (const key in secrets) {
       console.log(`storing secret for ${address}.${key}`);
@@ -36,17 +41,8 @@ export default {
       userAddress = user.address;
       retrieveAllSecrets = true;
     }
-    let storage;
-    if (config.storageConnection.includes('docdb.amazonaws.com')) {
-      storage = new Keyv(config.storageConnection, {
-        namespace: userAddress,
-        tlsCAFile: '/app/rds-combined-ca-bundle.pem',
-      });
-    } else {
-      storage = new Keyv(config.storageConnection, {
-        namespace: userAddress,
-      });
-    }
+    const storage = makeConnection(userAddress);
+
     let secrets: { [key: string]: any } = {};
     if (retrieveAllSecrets) {
       for await (const [key, value] of storage.iterator()) {
@@ -61,9 +57,8 @@ export default {
   deleteSecret: async (ctx: any) => {
     const { user } = ctx.state;
     const { key } = ctx.request.body;
-    const storage = new Keyv(config.storageConnection, {
-      namespace: user.address,
-    });
+    const storage = makeConnection(user.address);
+
     console.log('Deleting key...', key);
     await storage.delete(key);
     console.log('Deleted!', key);
