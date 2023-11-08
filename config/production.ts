a30452b74634743e8d53f23199963d13588c6b8c
Laurens Verspeek <l.verspeek@gmail.com>
2023-11-08 16:41:38 +0100

    update rpc url

--- config/production.ts
+++ config/production.ts
@@ -10,7 +10,7 @@ export default {
   storageConnection: STORAGE_CONNECTION || "sqlite://db.sqlite",
   blockchain: {
     solanaNet:
-      "https://lively-sparkling-shape.solana-mainnet.discover.quiknode.pro/515f35af4d64f05ab7b10cd8cd88f34f9d1ec7d0",
+      "https://rpc.hellomoon.io/853e30f5-383d-4cc6-a5ee-b5fb4c7a7178",
     nosTokenProgramId: "nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7",
     jobProgramId: "nosJhNRqr2bc9g1nfGDcXXTXvYUmxD4cVwy2pMWhrYM",
     stakeProgramId: "nosScmHY2uR24Zh751PmGj9ww9QRNHewh9H59AfrTJE",
